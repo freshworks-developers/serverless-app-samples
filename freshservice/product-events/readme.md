5de3a133d2eddecf8b8de8adb8279e7fa5cf86af
Kaustav Das Modak <kaustav.dasmodak@freshworks.com>
2021-10-12 10:45:26 +0530

    Set up event placeholders
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@freshworks.com>

--- freshservice/product-events/readme.md
+++ freshservice/product-events/readme.md
@@ -1,1 +1,21 @@
-## Serverless Product Events for Freshservice
+# Serverless Product Events for Freshservice
+
+This app demonstrates [product events available for Freshservice](https://developers.freshservice.com/docs/product-events/) serverless apps.
+
+## Available events
+
+Freshservice supports 3 product events:
+
+1. `onTicketCreate`: This event is triggered when a new ticket is created
+2. `onTicketUpdate`: This event is triggered when an existing ticket is updated
+3. `onConversationCreate`: This event is triggered when a note or a reply is added to an existing ticket
+
+Additionally, `onConversationCreate` is triggered for private or public notes and replies, coming in multiple sources. They way to identify the source is to look at the value of the `data.conversation.source` property in the event payload, and [map it to the Sources](server/server.js#L1-L14) as described in the documentation.
+
+## Setting up events
+
+In order to receive events, serverless apps need to do 2 things:
+
+1. Export a function from [`server/server.js`](server/server.js) that takes in a single argument for the `payload` sent by the event
+2. Declare the event and its handler in [`manifest.json`](manifest.json) under [`product.freshservice.events`](manifest.json#L3-L17)
+
--- freshservice/product-events/server/server.js
+++ freshservice/product-events/server/server.js
@@ -1,13 +1,47 @@
+/**
+ * Conversation sources for onConversationCreate events
+ */
+const CONVERSATION_SOURCES = {
+  0: "Reply",
+  2: "Note",
+  5: "Created from tweets",
+  6: "Created from survey feedback",
+  7: "Created from Facebook post",
+  8: "Created from forward email",
+  9: "Created from phone",
+  10: "Created from Mobihelp",
+  11: "E-commerce",
+};
+
 exports = {
+  /**
+   * Handler for onTicketCreate events. Triggered when a new ticket is created.
+   *
+   * @param {object} payload
+   */
+  onTicketCreateHandler(payload) {
+    console.log("onTicketCreate");
+    console.log(payload);
+  },
 
-  events: [
-    { event: 'onTicketCreate', callback: 'onTicketCreateHandler' }
-  ],
+  /**
+   * Handler for onTicketUpdate events. Triggered when a ticket is updated.
+   *
+   * @param {object} payload
+   */
+  onTicketUpdateHandler(payload) {
+    console.log("onTicketUpdate");
+    console.log(payload);
+  },
 
-  // args is a JSON block containing the payload information.
-  // args['iparam'] will contain the installation parameter values.
-  onTicketCreateHandler: function(args) {
-    console.log('Hello ' + args['data']['requester']['name']);
-  }
-
+  /**
+   * Handler for onConversationCreate events. Triggered when a reply or note (public or private) is added to a ticket.
+   *
+   * @param {object} payload
+   */
+  onConversationCreateHandler(payload) {
+    console.log("onConversationCreate");
+    console.log(`Source: ${CONVERSATION_SOURCES[payload.data.conversation.source]}`);
+    console.log(payload);
+  },
 };
