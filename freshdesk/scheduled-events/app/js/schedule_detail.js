a8f5ed9218c59ca62aaf9598419037945a3e167b
Saif Ali Shaik <saifshine7@gmail.com>
2021-11-28 17:47:09 +0530

    👌 IMPROVE: update manifest

--- freshdesk/scheduled-events/app/js/schedule_detail.js
+++ freshdesk/scheduled-events/app/js/schedule_detail.js
@@ -38,36 +38,39 @@ function validateFields() {
 }
 
 function getListOfSchedules(userId, callback) {
-  client.db.get(`schedules_${userId}`)
-  .then(function(data) {
-    callback(data);
-  }, function(err) {
-    if (err.status == 404) {
-      return callback({list:{}});
+  client.db.get(`schedules_${userId}`).then(
+    function (data) {
+      callback(data);
+    },
+    function (err) {
+      if (err.status == 404) {
+        return callback({ list: {} });
+      }
+      console.log(err);
+      sendNotification(DANGER_NOTIFICATION, `Error fetching list of schedules`);
     }
-    console.log(err);
-    sendNotification(DANGER_NOTIFICATION, `Error fetching list of schedules`);
-  });
+  );
 }
 
 function addScheduleToList(schedule, callback) {
-  getListOfSchedules(loggedInUserId, function(data) {
+  getListOfSchedules(loggedInUserId, function (data) {
     data.list[schedule.scheduleName] = schedule.subject;
-    client.db.set(`schedules_${loggedInUserId}`, data).then(function() {
-      callback();
-    }, function(err) {
-      console.log(err);
-      sendNotification(DANGER_NOTIFICATION, `Error storing list of schedules`);
-    });
+    client.db.set(`schedules_${loggedInUserId}`, data).then(
+      function () {
+        callback();
+      },
+      function (err) {
+        console.log(err);
+        sendNotification(DANGER_NOTIFICATION, `Error storing list of schedules`);
+      }
+    );
   });
 }
 
 function addListeners() {
-  q('#create-schedule').addEventListener('click', function() {
-
+  q('#create-schedule').addEventListener('click', function () {
     // Validate the values provided in the schedule creation form
     if (validateFields()) {
-
       // Generate an unique ID for storing the schedule
       var scheduleName = generateUuid();
 
@@ -79,32 +82,36 @@ function addListeners() {
         scheduleAtUTC: new Date(scheduleData.scheduleAt).toISOString()
       });
 
-      client.request.invoke('createSchedule', {
-        scheduleName: scheduleName,
-        scheduleData: scheduleData
-      }).then(function(data) {
-
-        // Persist the schedule name to the list post creation of schedule
-        addScheduleToList({
+      client.request
+        .invoke('createSchedule', {
           scheduleName: scheduleName,
-          subject: scheduleData.subject
-        }, function(err) {
-          sendNotification(SUCCESS_NOTIFICATION, 'Schedule created');
-          client.instance.close();
-        });
-
-      }, function(err) {
-        console.log(err);
-        sendNotification(DANGER_NOTIFICATION, `Error while creating schedule`);
-      });
+          scheduleData: scheduleData
+        })
+        .then(
+          function (data) {
+            // Persist the schedule name to the list post creation of schedule
+            addScheduleToList(
+              {
+                scheduleName: scheduleName,
+                subject: scheduleData.subject
+              },
+              function (err) {
+                sendNotification(SUCCESS_NOTIFICATION, 'Schedule created');
+                client.instance.close();
+              }
+            );
+          },
+          function (err) {
+            console.log(err);
+            sendNotification(DANGER_NOTIFICATION, `Error while creating schedule`);
+          }
+        );
     }
   });
 
-  q('#update-schedule').addEventListener('click', function() {
-
+  q('#update-schedule').addEventListener('click', function () {
     // Validate the values provided in the schedule creation form
     if (validateFields()) {
-
       var scheduleData = getFieldValues();
 
       Object.assign(scheduleData, {
@@ -113,26 +120,32 @@ function addListeners() {
         scheduleAtUTC: new Date(scheduleData.scheduleAt).toISOString()
       });
 
-      client.request.invoke('updateSchedule', {
-        scheduleName: currentUuid,
-        scheduleData: scheduleData
-      }).then(function(data) {
-
-        // Update the subject to the uuid in datastore
-        addScheduleToList({
+      client.request
+        .invoke('updateSchedule', {
           scheduleName: currentUuid,
-          subject: scheduleData.subject
-        }, function() {
-          sendNotification(SUCCESS_NOTIFICATION, 'Schedule updated');
-          client.instance.close();
-        });
-      }, function(err) {
-        console.log(err);
-        sendNotification(DANGER_NOTIFICATION, `Error while updating schedule`);
-      });
+          scheduleData: scheduleData
+        })
+        .then(
+          function (data) {
+            // Update the subject to the uuid in datastore
+            addScheduleToList(
+              {
+                scheduleName: currentUuid,
+                subject: scheduleData.subject
+              },
+              function () {
+                sendNotification(SUCCESS_NOTIFICATION, 'Schedule updated');
+                client.instance.close();
+              }
+            );
+          },
+          function (err) {
+            console.log(err);
+            sendNotification(DANGER_NOTIFICATION, `Error while updating schedule`);
+          }
+        );
     }
   });
-
 }
 
 function populateData(scheduleData) {
@@ -147,22 +160,21 @@ function populateData(scheduleData) {
   window.currentUuid = scheduleData.data.scheduleName;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   addListeners();
-  app.initialized().then(function(_client) {
+  app.initialized().then(function (_client) {
     window.client = _client;
 
-    client.data.get('loggedInUser').then(function(user) {
+    client.data.get('loggedInUser').then(function (user) {
       window.loggedInUserId = user.loggedInUser.id;
     });
 
-    client.instance.context().then(function(context){
+    client.instance.context().then(function (context) {
       if (context.data.newSchedule) {
-        q("#create-schedule").classList.remove('hidden');
-      }
-      else {
+        q('#create-schedule').classList.remove('hidden');
+      } else {
         populateData(context.data.scheduleData);
-        q("#update-schedule").classList.remove('hidden');
+        q('#update-schedule').classList.remove('hidden');
       }
     });
   });
